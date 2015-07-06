b30914f1b956e94bae7c54668a8d5c7aea4cd89c
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-06 17:14:05 +0100

    Added temp attribute to the thermostat. Like attr-accessor in ruby

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,7 +7,16 @@ describe('Thermostat', function() {
 
   describe('at the start, displays', function() {
     it('20', function() {
-      expect(thermostat.display).toEqual('20');
+      expect(thermostat.temp).toEqual(20);
     });
   });
+
+  describe()
+  //
+  // describe('can set temperature', function() {
+  //   it('up', function() {
+  //     expect(thermostat.set(30)).toEqual('20');
+  //   });
+  // });
+  //
 });
---
+++ src/thermostat.js
@@ -0,0 +1,12 @@
+function Thermostat() {
+  this.temp = 20;
+}
+// Thermostat.prototype.display = function() {
+//   return this.temp;
+// }
+
+
+//
+// Thermostat.prototype.display = function() {
+//     return("20");
+//   };
