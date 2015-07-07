b3deb8db15f9b2df5b92e4269233f5063ddc9daf
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-07 18:33:49 +0100

    Made colour/className settings into a function to run before displaying temp each time. Orange and green work. Red not working.

--- src/app.js
+++ src/app.js
@@ -4,23 +4,28 @@ document.getElementById("temp").innerHTML = thermostat.temp;
 
 document.getElementById("up").onclick = function(){
   thermostat.up();
+  colour();
   document.getElementById("temp").innerHTML = thermostat.temp;
 };
 
 document.getElementById("down").onclick = function(){
   thermostat.down();
+  colour();
   document.getElementById("temp").innerHTML = thermostat.temp;
 };
 
 document.getElementById("toggle").onchange = function(){
   thermostat.toggle();
+  colour();
   document.getElementById("temp").innerHTML = thermostat.temp;
 };
 
-if(thermostat.temp < 18) {
-  document.getElementById("temp").className = 'green';
-} else if(18 <= thermostat.temp < 25) {
-  document.getElementById("temp").className = 'yellow';
-} else if(thermostat.temp >= 25) {
-  document.getElementById("temp").className = 'red';
+colour = function(){
+  if(thermostat.temp < 18) {
+    document.getElementById("temp").className = 'green';
+  } else if(18 <= thermostat.temp < 25) {
+    document.getElementById("temp").className = 'orange';
+  } else if(thermostat.temp >= 25) {
+    document.getElementById("temp").className = 'red';
+  };
 };
