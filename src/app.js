cebf33b945cee8a1ed6c235690b2707a938349f5
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-08 12:03:20 +0100

    Change javascript bits to jquery.

--- src/app.js
+++ src/app.js
@@ -1,30 +1,50 @@
 var thermostat = new Thermostat();
+$( document ).ready(function() {
 
-document.getElementById("temp").innerHTML = thermostat.temp;
-
-document.getElementById("up").onclick = function(){
-  thermostat.up();
-  colour();
-  document.getElementById("temp").innerHTML = thermostat.temp;
-};
-
-document.getElementById("down").onclick = function(){
-  thermostat.down();
-  colour();
-  document.getElementById("temp").innerHTML = thermostat.temp;
-};
-
-document.getElementById("toggle").onchange = function(){
-  thermostat.toggle();
-  document.getElementById("temp").innerHTML = thermostat.temp;
-};
-
-colour = function(){
-  if(thermostat.temp < 18) {
-    document.getElementById("temp").className = 'green';
-  } else if(thermostat.temp < 25) {
-    document.getElementById("temp").className = 'orange';
-  } else {
-    document.getElementById("temp").className = 'red';
+  colour = function(){
+    if(thermostat.temp < 18) {
+      $("#temp").css("color", "green");
+    } else if((thermostat.temp >= 18) && (thermostat.temp < 25)) {
+      $("#temp").css("color", "orange");
+    } else if(thermostat.temp >= 25) {
+      $("#temp").css("color", "red");
+    };
   };
-};
+
+  function refreshTemp() {
+    colour();
+    $("#temp").html(thermostat.temp);
+  };
+
+  refreshTemp();
+
+  $("#up").click(function() {
+    thermostat.up();
+    refreshTemp();
+  });
+
+  $("#down").click(function() {
+    thermostat.down();
+    refreshTemp();
+  });
+
+  $("#toggle").change(function() {
+    thermostat.toggle();
+    refreshTemp();
+  });
+
+
+
+
+
+ // colour = function(){
+ //   if(thermostat.temp < 18) {
+ //     document.getElementById("temp").className = 'green';
+ //   } else if(thermostat.temp < 25) {
+ //     document.getElementById("temp").className = 'orange';
+ //   } else {
+ //     document.getElementById("temp").className = 'red';
+ //   };
+ // };
+
+});
